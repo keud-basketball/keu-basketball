e1d7d30e879ffaa88cb59017dc7c7b6d006602ee
keud-basketball <keudkeudtv@gmail.com>
2026-09-25 17:10:49 +0000

    Fix actual landscape VS layout

--- android/app/src/main/java/com/keu/hoopsofthelegends/MainActivity.java
+++ android/app/src/main/java/com/keu/hoopsofthelegends/MainActivity.java
@@ -1,5 +1,42 @@
 package com.keu.hoopsofthelegends;
 
+import android.content.pm.ActivityInfo;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+	private final Handler orientationHandler = new Handler(Looper.getMainLooper());
+	private final Runnable orientationWatcher = new Runnable() {
+		@Override
+		public void run() {
+			if (getBridge() != null && getBridge().getWebView() != null) {
+				getBridge().getWebView().evaluateJavascript(
+					"document.body.classList.contains('authenticated')",
+					value -> {
+						boolean authenticated = "true".equals(value);
+						setRequestedOrientation(authenticated
+							? ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE
+							: ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+					}
+				);
+			}
+			orientationHandler.postDelayed(this, 250);
+		}
+	};
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+		super.onCreate(savedInstanceState);
+		orientationHandler.post(orientationWatcher);
+	}
+
+	@Override
+	protected void onDestroy() {
+		orientationHandler.removeCallbacks(orientationWatcher);
+		super.onDestroy();
+	}
+}
